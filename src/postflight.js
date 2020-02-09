2f509a08e5dabefff36a7b9aa18957ba0a07ab8d
Marcus Foertsch <marcus@foertschemail.com>
2020-02-08 19:05:15 -0600

    Postflight module returns an object, not instance of a class

--- src/postflight.js
+++ src/postflight.js
@@ -1,10 +1,7 @@
 import ModelSpec from './model-spec';
 
-const Postflight = class {
-  constructor() {
-    this.specMap = new Map();
-  }
-
+const postflight = {
+  specMap: new Map(),
   addSpec(name, specConfig) {
     if (!name) {
       throw new Error('Spec must have a name');
@@ -13,11 +10,11 @@ const Postflight = class {
     const spec = new ModelSpec(specConfig.propertyMap, specConfig.modelClass);
 
     this.specMap.set(name, spec);
-  }
-
+  },
   getSpec(modelName) {
     return this.specMap.get(modelName);
-  }
+  },
+
 };
 
-export default new Postflight();
+export default postflight;
